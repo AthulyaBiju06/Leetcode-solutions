94a3f71c0965451e30f5a1c95733b5afbd7997ce
AthulyaBiju06 <athulyabijukumar307@gmail.com>
2026-02-18 22:25:04 +0530

    Create Search_position.c
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ Search_position.c
@@ -0,0 +1,22 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    
+    int left = 0;
+    int right = numsSize - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (nums[mid] == target) {
+            return mid;   // Target found
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;   // Search right half
+        }
+        else {
+            right = mid - 1;  // Search left half
+        }
+    }
+    
+    // If not found, left is the correct insert position
+    return left;
+}
